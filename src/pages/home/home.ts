7367db9f76235ab0e95209c0ebe9764c20f5e6e0
DESKTOP-GG7IC0M\Miguel <124miguel@gmail.com>
2017-04-28 17:22:51 -0500

    Nueva actualizacion Mike_28/04

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -24,7 +24,11 @@ export class HomePage {
    art_totales2: FirebaseListObservable<any[]>;
    art_totales : any;
    art_tot2: any;
-
+   mike_usuarios: FirebaseListObservable<any[]>;
+   aux: FirebaseListObservable<any[]>;
+   mike_cate: any;
+   mike_email: any;
+   postID: any;
   //variable para carousel
   art_c : any;
   //vals para filtro
@@ -33,16 +37,18 @@ export class HomePage {
   fecha: String = new Date().toISOString();
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
-  articulos: Array<{titulo: string, categoria: number, escirtorid: number,
-  escrito : string }>;
+  articulos: Array<{titulo: string, categoria: number, escirtorid: string,
+  escrito : string ,tipo: number, key: string}>;
   preserveSnapshot: true;
   constructor(private navCtrl: NavController, private af: AngularFire, private auth: AuthProvider,
   private loadingCtrl: LoadingController, public navParams: NavParams, public alertCtrl: AlertController) {
     this.searchControl = new FormControl();
     this.art_c=[];
-this.af.auth.subscribe(authData => {
-  this.mike = authData.uid;
-});
+
+  this.mike = this.af.auth.getAuth().auth.uid;
+  this.mike_email = this.af.auth.getAuth().auth.email;;
+
+
 
 ////MIKE
    ///Aqui es donde debe de ir la consulta a la base de datos con estos campos
@@ -54,26 +60,28 @@ this.af.auth.subscribe(authData => {
         ];*/
         this.art_tot2=[];
         this.art_totales2 = this.af.database.list('/articulos');
+        this.aux = this.af.database.list('/articulos');
+        this.mike_usuarios = this.af.database.list('/user');
+        
        this.art_totales2.subscribe(items => {
           this.art_tot2=[];
     // items is an array
     items.forEach(item => {
-        console.log('Item:', item.titulo);
-        
+        console.log('Item:', item.categoria);
             this.art_tot2.push({
               titulo: item.titulo,
                categoria: item.categoria, 
-               escirtorid: item.escirtorid, escrito : item.escrito}); 
+               escirtorid: item.escirtorid, 
+               escrito : item.escrito,
+               tipo: item.tipo,
+               key: item.key}); 
  
           });
+          
 
- console.log('tot',this.art_tot2);
-         this.art_totales=[
-      {titulo: 'dsada', categoria: 1, escirtorid: 1,escrito : 'dsadas'},
-      {titulo: 'iyqwoyrouwq', categoria: 1, escirtorid: 1,escrito : 'afsaji9'},
-      {titulo: 'popoe', categoria: 2, escirtorid: 1,escrito : 'skjdalsj'}
-         ];
-         console.log(this.art_totales, this.art_tot2);
+
+// console.log('tot',this.art_tot2);
+  //       console.log(this.art_totales, this.art_tot2);
          this.art_totales = this.art_tot2; 
        //S this.art_totales.subscribe.toString;
    for(let i = 1; i < 4; i++) {
@@ -84,20 +92,23 @@ this.af.auth.subscribe(authData => {
 
      this.articulos = this.art_totales;
       });
-        //this.art_tot2 = this.art_totales2;
-      /*  this.art_totales2.subscribe(elem =>{
-          elem.forEach(elem =>{
-            console.log(elem);
+
+      this.mike_usuarios.subscribe(items2 => {
+    items2.forEach(item2 => {
+        console.log('mike2.5 :', item2.categoria, item2.email, this.mike_email, this.mike);
+        if  (this.mike_email == item2.email){
+          this.mike_cate = item2.categoria;
+          console.log("correo actual"+this.mike_email+"Correo item "+item2.email+this.mike_cate);
+        }
+          
           });
-        });*/
-        //this.art_totales = this.art_tot2;
-       
+        
+      }); 
+
+        
     
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     'american-football', 'boat', 'bluetooth', 'build'];
-
-
-
 
   }
  
@@ -129,12 +140,14 @@ this.af.auth.subscribe(authData => {
 
 }
   filterItems(searchTerm){
-
+    
 	    return this.articulos.filter((articulos) => {
 	    	return articulos.titulo.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
 	    });	
     }	
     	onSearchInput(){
+        console.log("hjashd");
+        this.setFilteredItems();
 		this.searching = true;
 	}
 
@@ -233,6 +246,10 @@ presentPrompt() {
         name: 'categoria',
         placeholder: 'categoria 1: 2: 3: 4:',
         type: 'number'
+      },  {
+        name: 'tipo',
+        placeholder: 'tipo 1: 2: 3: 4:',
+        type: 'number'
       }
     ],
     buttons: [
@@ -261,8 +278,10 @@ presentPrompt() {
             //this.articulos = this.art_totales;
           
             
-            this.art_totales2.push({titulo: dataP.titulo, categoria: dataP.categoria, escirtorid: this.mike, escrito : dataP.escrito});
-            
+            var newPostRef = this.art_totales2.push({titulo: dataP.titulo, categoria: dataP.categoria, escirtorid: this.mike, escrito : dataP.escrito, tipo : dataP.tipo, key: "hola"});
+            this.postID = newPostRef.key;
+            console.log(this.postID);
+            this.updatekey(this.postID);
       } else {
             // invalid login
             return false;
@@ -303,23 +322,80 @@ presentPrompt() {
   prompt.present();
 }
 mostrar(artid){
-   if(this.mike == artid.escirtorid){
+   if(this.mike == artid.escirtorid && this.mike_cate == 2){
      return 1;
-   }else{
+   }else if(this.mike_cate == 5){
+     return 1;
+   }
+   else{
     return 0;
    }
-   //console.log('esntrp',artid);
-  // return 1;
-
+ }
+ mostrar2(){
+    if(this.mike_cate == 2 || this.mike_cate == 5){
+      return 1;
+    }
+    else {
+      return 0;
+    }
  }
 
+ mostrar43(artid){
+
+   switch (this.mike_cate) {
+            case "1":
+                if(this.mike_cate == artid.tipo){
+                  return 1;
+                }else{
+                  return 0;
+                }
+            case "2" :
+                if(this.mike_cate == artid.tipo || artid.tipo == 1){
+                  return 1;
+                }else{
+                  return 0;
+                }
+                
+            case "3":
+                if(this.mike_cate == artid.tipo || artid.tipo == 1){
+                  return 1;
+                }else{
+                  return 0;
+                }
+            case "4":
+                if(this.mike_cate == artid.tipo || artid.tipo == 1){
+                  return 1;
+                }else{
+                  return 0;
+                }
+            case "5":
+                if(this.mike_cate != 0){
+                  return 1;
+                }else{
+                  return 0;
+                }
+            
+
+        }
+   
+ }
+ eliminar(articulo){
+   console.log("se elimino",articulo.key);
+   this.art_totales2.remove(articulo.key);
+}
+updatekey(keym){
+this.art_totales2.update(keym,{
+							key: keym
+						});          
+}
   logout() {
     let loading = this.loadingCtrl.create({
       content: 'Espera por favor...'
     });
     loading.present();
-
+    this.af.auth.logout();
     this.auth.logout;
+    this.af.auth.logout();
       setTimeout(() => {
         loading.dismiss();
         this.navCtrl.setRoot(AuthPage);
